b8931fcd28603e4e22b58c0f36370caef700116a
Thidtanai <thidtanai_k@cmu.ac.th>
2024-05-30 18:05:18 +0700

    Update_BST

--- Kong/JS_DataStruct&Algorithm/Node.js
+++ Kong/JS_DataStruct&Algorithm/Node.js
@@ -1,8 +1,16 @@
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-module.exports = Node;
+class BstNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+module.exports = { Node, BstNode };
