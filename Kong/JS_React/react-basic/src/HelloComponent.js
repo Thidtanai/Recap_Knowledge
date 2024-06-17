38eb28427454f4bd4f7394d73d15d49410404887
Thidtanai <thidtanai_k@cmu.ac.th>
2024-06-17 19:50:41 +0700

    Update_component

---
+++ Kong/JS_React/react-basic/src/HelloComponent.js
@@ -0,0 +1,6 @@
+// การสร้าง external component
+function HelloComponent(){
+    return <h1>Hello Component</h1>
+}
+
+export default HelloComponent
--- Kong/JS_React/react-basic/src/index.js
+++ Kong/JS_React/react-basic/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HelloComponent from "./HelloComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//การสร้าง function component
+// function HelloComponent() {
+//   return <h1>Hello Component</h1>;
+// }
+
+//การสร้าง class component
+// class HelloComponent extends React.Component{
+//   render(){
+//     return <h1>Hello Component</h1>
+//   }
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HelloComponent />);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
