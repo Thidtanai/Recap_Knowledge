6b7fcea14cf3f6b72c3e541202966ad97faec9ea
Thidtanai <thidtanai_k@cmu.ac.th>
2024-06-21 19:53:33 +0700

    Update_PropTypes

--- Kong/JS_React/react-basic/src/components/Item.js
+++ Kong/JS_React/react-basic/src/components/Item.js
@@ -1,11 +1,18 @@
+import PropTypes from "prop-types"; //For validate props type
+
 /* Method1 */
 const Item = (props) => {
-  const {title, amount} = props;
+  const { title, amount } = props;
   return (
     <li>
       {title} <span>{amount}</span>
     </li>
   );
+};
+
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
 };
 
 /* Method2 */
--- Kong/JS_React/react-basic/src/components/Transaction.js
+++ Kong/JS_React/react-basic/src/components/Transaction.js
@@ -1,18 +1,21 @@
 import Item from "./Item";
 import "./Transaction.css";
+import { v4 as uuidv4 } from "uuid";  //Use id for key
 
 const Transaction = () => {
   const data = [
-    { title: "ค่ารักษาพยาบาล", amount: "2000" },
-    { title: "เงินเดือน", amount: "50000" },
-    { title: "ค่าเดินทาง", amount: "200" },
-    { title: "ค่าเช่าห้อง", amount: "3000" },
+    { title: "ค่ารักษาพยาบาล", amount: 2000 },
+    { title: "เงินเดือน", amount: 50000 },
+    { title: "ค่าเดินทาง", amount: 200 },
+    { title: "ค่าเช่าห้อง", amount: 3000 },
+    { title: "ค่ารถ", amount: 6000 },
   ];
   return (
     <ul className="item-list">
-      <Item title={data[0].title} amount={data[0].amount} />
-      <Item title={data[1].title} amount={data[1].amount} />
-      <Item title={data[2].title} amount={data[2].amount} />
+      {data.map((element) => {
+        //return <Item title={element.title} amount={element.amount}/>
+        return <Item {...element} key={uuidv4()} />;
+      })}
     </ul>
   );
 };
