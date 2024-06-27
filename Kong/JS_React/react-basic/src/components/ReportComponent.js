f6ef7ad90b4aa8fbeb5723ead364fe3d57c8ccc2
thidtanai <thidtanai_k@cmu.ac.th>
2024-06-27 17:29:20 +0700

    Update_RegEx

--- Kong/JS_React/react-basic/src/components/ReportComponent.js
+++ Kong/JS_React/react-basic/src/components/ReportComponent.js
@@ -5,6 +5,10 @@ import "./ReportComponent.css";
 const ReportComponent = () => {
   /* Consumer */
   const { income, expense } = useContext(DataContext);
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
   return (
     <div>
       {/* <DataContext.Consumer>
@@ -15,15 +19,15 @@ const ReportComponent = () => {
         )}
       </DataContext.Consumer> */}
       <h4>Total (baht)</h4>
-      <h1>฿{(income - expense).toFixed(2)}</h1>
+      <h1>฿{formatNumber((income - expense).toFixed(2))}</h1>
       <div className="report-container">
         <div>
           <h4>Income</h4>
-          <p className="report plus">฿{income}</p>
+          <p className="report plus">฿{formatNumber(income)}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p className="report minus">฿{expense}</p>
+          <p className="report minus">฿{formatNumber(expense)}</p>
         </div>
       </div>
     </div>
