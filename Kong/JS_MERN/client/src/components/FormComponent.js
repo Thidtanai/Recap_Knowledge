45c4d65438846c4e9165f968e7b0eb21f1952c3f
thidtanai <thidtanai_k@cmu.ac.th>
2024-07-18 18:50:53 +0700

    Update RouterDom: install react-router-dom and change path route from App to MyRoute

---
+++ Kong/JS_MERN/client/src/components/FormComponent.js
@@ -0,0 +1,13 @@
+/**
+ * จัดเก็บหน้าตา Form
+ */
+
+const FormComponent = () => {
+  return (
+    <div className="container">
+      <h1> Form Component </h1>
+    </div>
+  );
+};
+
+export default FormComponent;
