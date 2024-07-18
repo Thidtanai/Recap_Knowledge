8c6d75856f58f1ac79fb025db5ca725177a3d92c
thidtanai <thidtanai_k@cmu.ac.th>
2024-07-18 19:28:35 +0700

    Update: create form state

--- Kong/JS_MERN/client/src/components/FormComponent.js
+++ Kong/JS_MERN/client/src/components/FormComponent.js
@@ -2,22 +2,49 @@
  * จัดเก็บหน้าตา Form
  */
 
+import { useState } from "react";
+
 const FormComponent = () => {
+  const [state, setState] = useState({
+    title: "",
+    content: "",
+    author: "",
+  });
+  const { title, content, author } = state;
+
+  const inputValue = (name) => (event) => {
+    setState({ ...state, [name]: event.target.value });
+  };
+
   return (
     <div className="container p-5">
       <h1>เขียนบทความ</h1>
       <form>
         <div className="form-group">
           <label>ชื่อบทความ</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={inputValue("title")}
+          />
         </div>
         <div className="form-group">
           <label>รายละเอียด</label>
-          <textarea className="form-control"></textarea>
+          <textarea
+            className="form-control"
+            value={content}
+            onChange={inputValue("content")}
+          ></textarea>
         </div>
         <div className="form-group">
           <label>ผู้แต่ง</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={author}
+            onChange={inputValue("author")}
+          />
         </div>
         <br />
         <input type="submit" value="บันทึก" className="btn btn-primary" />
