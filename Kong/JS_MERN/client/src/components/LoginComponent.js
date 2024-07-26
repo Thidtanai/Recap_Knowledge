d5d9c03ad42b79301339f3f2db94f74671817fa0
Thidtanai <thidtanai_k@cmu.ac.th>
2024-07-26 19:37:25 +0700

    Update Login page

---
+++ Kong/JS_MERN/client/src/components/LoginComponent.js
@@ -0,0 +1,50 @@
+import NavbarComponent from "./NavbarComponent";
+import { useState } from "react";
+
+const LoginComponent = () => {
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = state;
+
+  const inputValue = (name) => (event) => {
+    setState({ ...state, [name]: event.target.value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log({ username, password });
+  };
+
+  return (
+    <div className="container p-5">
+      <NavbarComponent />
+      <h1>เข้าสู่ระบบ | Admin</h1>
+      <form onSubmit={submitForm}>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            value={username}
+            onChange={inputValue("username")}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={inputValue("password")}
+          />
+        </div>
+        <br />
+        <input type="submit" value="เข้าสู่ระบบ" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+};
+
+export default LoginComponent;
