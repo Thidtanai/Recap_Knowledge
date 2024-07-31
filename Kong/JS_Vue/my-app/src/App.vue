1adb18336c38f121ba9584940a82efb1e8572ea3
Thidtanai <thidtanai_k@cmu.ac.th>
2024-07-31 22:26:45 +0700

    Update method

--- Kong/JS_Vue/my-app/src/App.vue
+++ Kong/JS_Vue/my-app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <h1>Kong</h1>
+    <!-- Interporation ดึงตัวแปรจาก data มาใช้งาน -->
+    <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
+    <h1>อายุ {{ age }} ปี</h1>
+    <!-- v-html -->
+    <p>ที่อยู่ <span v-html="address"></span></p>
   </section>
 
 </template>
@@ -9,6 +13,19 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      firstName: "Kong",
+      lastName: "Ruksiam",
+      age: 15,
+      address: "<strong>กรุงเทพ</strong>"
+    }
+  }, methods: {
+    getFullName() {
+      //return this.firstName + " " + this.lastName
+      return `${this.firstName + " " + this.lastName}`
+    }
+  }
 
 }
 </script>
