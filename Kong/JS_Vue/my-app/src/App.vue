6360f975645709b89d56ca0f362a3ea6fcb9a0aa
thidtanai <thidtanai_k@cmu.ac.th>
2024-08-05 09:12:15 +0700

    Update Attribute binding

--- Kong/JS_Vue/my-app/src/App.vue
+++ Kong/JS_Vue/my-app/src/App.vue
@@ -1,33 +1,41 @@
 <template>
   <section>
+    <!-- Attribute binding -->
+    <!-- <img v-bind:src="picture" v-bind:width="size" v-bind:height="size" /> -->
+    <img :src="picture" :width="size" :height="size" />
+
     <!-- Interporation ดึงตัวแปรจาก data มาใช้งาน -->
     <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
     <h1>อายุ {{ age }} ปี</h1>
+
     <!-- v-html -->
     <p>ที่อยู่ <span v-html="address"></span></p>
-  </section>
 
+    <a :href="social" target="_blank">Facebook</a>
+  </section>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       firstName: "Kong",
       lastName: "Ruksiam",
       age: 15,
-      address: "<strong>กรุงเทพ</strong>"
-    }
-  }, methods: {
+      address: "<strong>กรุงเทพ</strong>",
+      picture: "https://cdn-icons-png.flaticon.com/512/3685/3685367.png",
+      size: 50,
+      social: "https://www.facebook.com/Thidtanai-Kaewphet",
+    };
+  },
+  methods: {
     getFullName() {
       //return this.firstName + " " + this.lastName
-      return `${this.firstName + " " + this.lastName}`
-    }
-  }
-
-}
+      return `${this.firstName + " " + this.lastName}`;
+    },
+  },
+};
 </script>
 
 <style>
