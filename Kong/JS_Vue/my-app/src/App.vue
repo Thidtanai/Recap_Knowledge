90f30962865dad2f4e4df8255fb287305bad9c31
thidtanai <thidtanai_k@cmu.ac.th>
2024-08-05 10:29:57 +0700

    Update Ref DOM El

--- Kong/JS_Vue/my-app/src/App.vue
+++ Kong/JS_Vue/my-app/src/App.vue
@@ -2,16 +2,44 @@
   <section>
     <!-- Attribute binding -->
     <!-- <img v-bind:src="picture" v-bind:width="size" v-bind:height="size" /> -->
-    <img :src="picture" :width="size" :height="size" />
+    <img :src="picture" :width="size" :height="size" ref="imageEl" /><br />
+
+    <!-- ป้อนชื่อเล่น : <input type="text" v-on:input="setNickName" /> -->
+    <form @submit.prevent="submitForm">
+      <lable>ป้อนชื่อเล่น: </lable>
+      <!-- <input type="text" v-on:input="setNickName" ref="nickNameEl" /> -->
+      <input type="text" ref="nickNameEl" />
+      <button type="submit">บันทึก</button>
+    </form>
 
     <!-- Interporation ดึงตัวแปรจาก data มาใช้งาน -->
     <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
-    <h1>อายุ {{ age }} ปี</h1>
+    <h1>ชื่อเล่น : {{ nickName }}</h1>
+    <h1>อายุ : {{ age }} ปี</h1>
 
     <!-- v-html -->
-    <p>ที่อยู่ <span v-html="address"></span></p>
+    <p>ที่อยู่ : <span v-html="address"></span></p>
 
-    <a :href="social" target="_blank">Facebook</a>
+    <p>Social: <a :href="social" target="_blank">Facebook</a></p>
+    <p>งานอดิเรก:</p>
+    <ul>
+      <li>{{ hobby[0] }}</li>
+      <li>{{ hobby[1] }}</li>
+      <li>{{ hobby[2] }}</li>
+    </ul>
+    <p>ข้อมูลพื้นฐาน:</p>
+    <ul>
+      <li>เพศ: {{ general.gender }}</li>
+      <li>น้ำหนัก: {{ general.weight }} kg</li>
+      <li>ส่วนสูง: {{ general.height }} cm</li>
+      <li>โรคประจำตัว: {{ general.status }}</li>
+    </ul>
+
+    <!-- Event -->
+    <!-- <button v-on:click="showData">คลิกเพื่อดูข้อมูล</button> -->
+    <button @click="showData()">คลิกเพื่อดูข้อมูล</button>
+    <button @click.ctrl="increment(10)">เพิ่ม</button>
+    <button @click.middle="decrement(5)">ลด</button>
   </section>
 </template>
 
@@ -22,17 +50,42 @@ export default {
     return {
       firstName: "Kong",
       lastName: "Ruksiam",
+      nickName: "",
       age: 15,
       address: "<strong>กรุงเทพ</strong>",
       picture: "https://cdn-icons-png.flaticon.com/512/3685/3685367.png",
       size: 50,
       social: "https://www.facebook.com/Thidtanai-Kaewphet",
+      hobby: ["ทำสวน", "เล่นเกม", "ฟังเพลง"],
+      general: {
+        gender: "ชาย",
+        weight: 70.4,
+        height: 170,
+        status: false,
+      },
     };
   },
   methods: {
     getFullName() {
       //return this.firstName + " " + this.lastName
       return `${this.firstName + " " + this.lastName}`;
+    },
+    showData() {
+      alert(this.firstName);
+    },
+    increment(value) {
+      this.age += value;
+    },
+    decrement(value) {
+      this.age -= value;
+    },
+    // setNickName(event) {
+    //   this.nickName = event.target.value;
+    // },
+    submitForm() {
+      //e.preventDefault();
+      alert("บันทึกชื่อเล่นเรียบร้อย");
+      this.nickName = this.$refs.nickNameEl.value;
     },
   },
 };
