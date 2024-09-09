c37fc92b0f7053db2980fd7a235bc6a21e0fd9ee
thidtanai <thidsloth@gmail.com>
2024-09-09 11:33:54 +0700

    Update Vue: Style components

--- Kong/JS_Vue/my-app-component/src/components/ListData.vue
+++ Kong/JS_Vue/my-app-component/src/components/ListData.vue
@@ -2,10 +2,13 @@
   <div>
     <ul>
       <PersonComponent
-        v-for="(item, index) in employees"
-        :key="index"
+        v-for="item in employees"
+        :id="item.id"
+        :key="item.id"
         :name="item.name"
         :salary="item.salary"
+        :department="item.department"
+        :isVisible="item.isVisible"
       />
     </ul>
   </div>
@@ -19,9 +22,54 @@ export default {
   components: {
     PersonComponent,
   },
-  props: ["employees"],
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Kong",
+          salary: 40000,
+          department: "Programmer",
+          isVisible: false,
+        },
+        {
+          id: 2,
+          name: "JoJo",
+          salary: 30000,
+          department: "Business",
+          isVisible: false,
+        },
+        {
+          id: 3,
+          name: "Gam",
+          salary: 20000,
+          department: "Programmer",
+          isVisible: false,
+        },
+        {
+          id: 4,
+          name: "Sompong",
+          salary: 15000,
+          department: "Graphic Designer",
+          isVisible: false,
+        },
+        {
+          id: 5,
+          name: "Malee",
+          salary: 18000,
+          department: "Seller",
+          isVisible: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
 </style>
--- Kong/JS_Vue/my-app-component/src/components/PersonComponent.vue
+++ Kong/JS_Vue/my-app-component/src/components/PersonComponent.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>ชื่อ: {{ name }} เงินเดือน: {{ salary }}</h1>
-  </div>
+  <li>
+    <h1>{{ name }}</h1>
+    <button>ดูรายละเอียด</button> &nbsp;
+    <button>ลบข้อมูล</button>
+    <div v-show="isVisible">
+      <p>เงินเดือน: {{ salary }} บาท , ตำแหน่งงาน: {{ department }}</p>
+    </div>
+  </li>
 </template>
 
 <script>
@@ -13,6 +18,9 @@ export default {
     };
   },
   props: {
+    id: {
+      type: Number,
+    },
     name: {
       type: String,
       required: true,
@@ -21,13 +29,35 @@ export default {
       type: Number,
       default: 15000,
     },
+    department: {
+      type: String,
+      required: true,
+    },
+    isVisible: {
+      type: Boolean,
+    },
   },
 };
 </script>
 
 <!-- scoped เพื่อให้มีผลแค่ component นี้ -->
 <style scoped>
-h1 {
-  color: red;
+li {
+  margin: 1rem 0;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background: #8ddba4;
+  padding: 0.5rem;
+  color: #1f1f1f;
+  border-radius: 25px;
+}
+button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
 </style>
