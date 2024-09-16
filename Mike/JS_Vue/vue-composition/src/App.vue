46dfac70b4a8108cbe359819be353bfc1fc47efb
thidtanai <thidsloth@gmail.com>
2024-09-16 16:25:11 +0700

    Init Mike_JS_Vue-composition API

---
+++ Mike/JS_Vue/vue-composition/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { reactive, ref } from "vue";
+import FormComponent from "./components/FormComponent.vue";
+
+/*  Binding  */
+// const message = "Hello world";
+const counterRef = ref(0);
+const counterReactive = reactive({ count: 0 });
+const searchText = ref("v-model");
+const eventName = ref("event");
+
+const increment = () => {
+  counterRef.value++;
+  counterReactive.count++;
+};
+
+const eventInput = (event) => {
+  eventName.value = event.target.value;
+};
+
+/*  Condition */
+const isCheck = ref(false);
+
+const toggleCheck = () => {
+  isCheck.value = !isCheck.value;
+};
+
+/*  Loop  */
+const items = ref(["item1", "item2", "item3"]);
+</script>
+
+
+<template>
+  <div>
+    <!-- Binding -->
+    <div>
+      <div>Hello Ref: {{ counterRef }}</div>
+      <div>Hello Reactive : {{ counterReactive.count }}</div>
+      <button @click="increment">Add counter</button>
+      <div>{{ searchText }}</div>
+      <input type="text" v-model="searchText" />
+      <div>{{ eventName }}</div>
+      <input type="text" @input="eventInput" />
+    </div>
+
+    <!-- Condition -->
+    <div>
+      <div v-if="isCheck">isCheck</div>
+      <div v-else>notCheck</div>
+      <button @click="toggleCheck()">toggle</button>
+    </div>
+
+    <!-- Loop -->
+    <div>
+      <ul>
+        <li v-for="(item, index) in items" key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+
+    <!-- Workshop -->
+    <form-component />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ Mike/JS_Vue/vue-composition/src/components/FormComponent.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { reactive } from "vue";
+
+const genderList = ["Male", "Female", "Other"];
+const interestList = ["Book", "Sport", "Music"];
+
+const formData = reactive({
+  interests: [],
+});
+
+const submitForm = () => {
+  console.log(formData)
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>First name</div>
+      <input type="text" name="firstname" v-model="formData.firstName" />
+    </div>
+    <div>
+      <div>Last name</div>
+      <input type="text" name="lastname" v-model="formData.lastName" />
+    </div>
+    <div>
+      <div>Gender</div>
+      <div v-for="gender in genderList" :key="gender">
+        <input
+          type="radio"
+          name="gender"
+          :value="gender"
+          v-model="formData.gender"
+        />
+        {{ gender }}
+      </div>
+    </div>
+    <div>
+      <div>Interest</div>
+      <div v-for="interest in interestList" :key="interest">
+        <input
+          type="checkbox"
+          name="interest"
+          :value="interest"
+          v-model="formData.interests"
+        />
+        {{ interest }}
+      </div>
+    </div>
+    <div>
+      <div>Description</div>
+      <textarea
+        v-model="formData.description"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+    <button @click="submitForm()">submit</button>
+  </div>
+</template>
+
+<style>
+</style>
